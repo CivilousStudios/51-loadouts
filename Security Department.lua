03ff8f0a4f7ee53dc6bdbc654ce2e13fa95becc6
Fwogg3r <73174497+Fwogg3r@users.noreply.github.com>
2020-10-20 08:57:39 -0400

    Create Security Department.lua

---
+++ Security Department.lua
@@ -0,0 +1,12 @@
+return {
+	[1] = {Tools = {"P90", "Glock 18", "Bandage", "Mop"}, Morph = "Recruit+", Clothing = {5741888886, 5741895810}, HealthBonus = 10, TeamtagName = "Security Recruit"},
+	[2] = {Tools = {"P90", "Glock 18", "Bandage", "Mop"}, Morph = "Recruit+", Clothing = {5741888886, 5741895810}, HealthBonus = 15, TeamtagName = "Security Guard"},
+	[3] = {Tools = {"P90", "MP5", "Glock 18", "Bandage", "Mop"}, Morph = "Guard+", Clothing = {5741888886, 5741895810}, HealthBonus = 20, TeamtagName = "Security Corporal"},
+	[4] = {Tools = {"P90", "MP5", "Glock 18", "Bandage", "Mop"}, Morph = "Guard+", Clothing = {5741888886, 5741895810}, HealthBonus = 25, TeamtagName = "Security Agent"},
+	[5] = {Tools = {"P90", "MP5", "Glock 18", "Bandage", "Medkit", "Mop"}, Morph = "Guard+", Clothing = {5741888886, 5741895810}, HealthBonus = 30, TeamtagName = "Security Sergeant"},
+	[6] = {Tools = {"P90 SOW", "MP5", "Glock 18", "Bandage", "Medkit", "Detain", "Mop"}, Morph = "Officer", Clothing = {5741888886, 5741895810}, HealthBonus = 35, TeamtagName = "Security Officer"},
+	[7] = {Tools = {"P90 SOW", "MP5", "Glock 18", "Bandage", "Medkit", "Detain", "Mop"}, Morph = "Officer+", Clothing = {5741888886, 5741895810}, HealthBonus = 40, TeamtagName = "Security Lieutenant"},
+	[8] = {Tools = {"P90 SOW", "MP5", "Glock 18", "Bandage", "Medkit", "Detain", "Mop"}, Morph = "Officer+", Clothing = {5741888886, 5741895810}, HealthBonus = 50, TeamtagName = "Security Captain"},
+	[9] = {Tools = {"P90 Elite", "MP5", "Glock 18", "Bandage", "Medkit", "Detain", "Mop"}, Morph = "SRT", Clothing = {5741888886, 5741895810}, HealthBonus = 75, TeamtagName = "Security Major"},
+	[10] = {Tools = {"P90 Elite", "MP249", "MP5", "Glock 18", "Bandage", "Detain", "Medkit", "Mop"}, Morph = "SRTL", Clothing = {5741888886, 5741895810}, HealthBonus = 100, TeamtagName = "Security Chief"},
+}
