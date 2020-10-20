a14d9bdd09f10f1697f427671fae0dcbc05de6f8
Fwogg3r <73174497+Fwogg3r@users.noreply.github.com>
2020-10-20 08:55:14 -0400

    Create Moderation Team.lua

---
+++ Moderation Team.lua
@@ -0,0 +1,7 @@
+return {
+	[1] = {Tools = {}, Morph = "", Clothing = {559097145, 559098341}, HealthBonus = 0, TeamtagName = "Exploiter"},
+	[2] = {Tools = {"Railgun", "Detain", "Medkit", "Torch", "Coolant"}, Morph = "", Clothing = {559097145, 559098341}, HealthBonus = 0, TeamtagName = "Helper"},
+	[3] = {Tools = {"Railgun", "Detain", "Medkit", "Torch", "Coolant"}, Morph = "", Clothing = {559097145, 559098341}, HealthBonus = 0, TeamtagName = "Moderator"},
+	[4] = {Tools = {"Railgun", "Detain", "Medkit", "Torch", "Coolant"}, Morph = "", Clothing = {559097145, 559098341}, HealthBonus = 0, TeamtagName = "Administrator"},
+	[5] = {Tools = {"Railgun", "Detain", "Medkit", "Torch", "Coolant"}, Morph = "", Clothing = {559097145, 559098341}, HealthBonus = 0, TeamtagName = "Owner"},
+}
