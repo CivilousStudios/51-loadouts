a94b7cc85e14cf4935bc611c817ffec20c604589
Fwogg3r <73174497+Fwogg3r@users.noreply.github.com>
2020-10-20 08:54:43 -0400

    Create Mobile Task Forces.lua

---
+++ Mobile Task Forces.lua
@@ -0,0 +1,12 @@
+return {
+	[1] = {Tools = {"M4", "Glock 18", "Bandage", "Flashlight", "Mop"}, Morph = "Beta-7", Clothing = {5743698083, 5743706495}, HealthBonus = 0, TeamtagName = "Recruit"},
+	[2] = {Tools = {"M4", "Glock 18", "Bandage", "Flashlight", "Mop"}, Morph = "Beta-7", Clothing = {5743698083, 5743706495}, HealthBonus = 5, TeamtagName = "Trainee"},
+	[3] = {Tools = {"M4", "Glock 18", "Bandage", "Flashlight", "Mop"}, Morph = "Sigma-9", Clothing = {5743698083, 5743706495}, HealthBonus = 10, TeamtagName = "Junior Operative"},
+	[4] = {Tools = {"M4", "Glock 18", "Bandage", "Flashlight", "Mop"}, Morph = "Sigma-9", Clothing = {5743698083, 5743706495}, HealthBonus = 15, TeamtagName = "Operative"},
+	[5] = {Tools = {"G36", "Glock 18", "Bandage", "Flashlight", "Mop"}, Morph = "Sigma-9", Clothing = {5743698083, 5743706495}, HealthBonus = 20, TeamtagName = "Corporal"},
+	[6] = {Tools = {"G36", "Glock 18", "Bandage", "Flashlight", "Detain", "Mop"}, Morph = "Sigma-9", Clothing = {5743698083, 5743706495}, HealthBonus = 25, TeamtagName = "Sergeant"},
+	[7] = {Tools = {"G36", "Glock 18", "Bandage", "Flashlight", "Detain", "Mop"}, Morph = "Iota-10", Clothing = {5743698083, 5743706495}, HealthBonus = 30, TeamtagName = "Officer"},
+	[8] = {Tools = {"G36 Elite", "Glock 18", "Bandage", "Flashlight", "Detain", "Mop"}, Morph = "Iota-10", Clothing = {5743698083, 5743706495}, HealthBonus = 50, TeamtagName = "Lieutenant"},
+	[9] = {Tools = {"G36 Elite", "Glock 18", "Bandage", "Flashlight", "Detain", "Mop"}, Morph = "TFL+", Clothing = {5741888508, 5741894642}, HealthBonus = 75, TeamtagName = "Captain"},
+	[10] = {Tools = {"G36 Elite", "M60", "Bandage", "Flashlight", "Detain", "Mop"}, Morph = "COM+", Clothing = {5741888508, 5741894642}, HealthBonus = 100, TeamtagName = "Commander"},
+}
