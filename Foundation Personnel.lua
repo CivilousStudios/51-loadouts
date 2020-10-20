fd246ba04c796ac8f84c070bb6952103fa8a060f
Fwogg3r <73174497+Fwogg3r@users.noreply.github.com>
2020-10-20 08:50:20 -0400

    Create Foundation Personnel.lua

---
+++ Foundation Personnel.lua
@@ -0,0 +1,12 @@
+return {
+	[1] = {Tools = {"Mop"}, Morph = "", Clothing = {5743734671, 5743736618}, HealthBonus = 0, TeamtagName = "Janitorial Intern"},
+	[2] = {Tools = {"Mop", "Torch"}, Morph = "", Clothing = {5743734671, 5743736618}, HealthBonus = 5, TeamtagName = "Janitorial Employee"},
+	[3] = {Tools = {"Mop", "Torch"}, Morph = "", Clothing = {276924405, 276924616}, HealthBonus = 10, TeamtagName = "Foundation Intern"},
+	[4] = {Tools = {"Mop", "Torch"}, Morph = "", Clothing = {276924405, 276924616}, HealthBonus = 15, TeamtagName = "Foundation Employee"},
+	[5] = {Tools = {"Glock 17","Mop", "Torch"}, Morph = "", Clothing = {276924438, 276924616}, HealthBonus = 20, TeamtagName = "Foundation Agent"},
+	[6] = {Tools = {"Glock 17","Mop", "Torch"}, Morph = "", Clothing = {276924438, 276924616}, HealthBonus = 25, TeamtagName = "Foundation Senior Agent"},
+	[7] = {Tools = {"MP5K","Glock 17", "Mop", "Torch"}, Morph = "", Clothing = {276924405, 276924616}, HealthBonus = 30, TeamtagName = "Foundation Supervisor"},
+	[8] = {Tools = {"MP5","Glock 17", "Mop", "Torch"}, Morph = "", Clothing = {276924405, 276924616}, HealthBonus = 50, TeamtagName = "Foundation Executive"},
+	[9] = {Tools = {"MP5","Glock 17", "Mop", "Torch"}, Morph = "", Clothing = {276924405, 276924616}, HealthBonus = 75, TeamtagName = "Foundation Board"},
+	[10] = {Tools = {"MP5","Glock 17", "Mop", "Torch", "Detain"}, Morph = "", Clothing = {276924405, 276924616}, HealthBonus = 100, TeamtagName = "Foundation Attendant"},
+}
