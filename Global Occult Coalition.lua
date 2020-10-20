78c0d6029c4320894d429b4254039795211bd414
Fwogg3r <73174497+Fwogg3r@users.noreply.github.com>
2020-10-20 08:51:04 -0400

    Create Global Occult Coalition.lua

---
+++ Global Occult Coalition.lua
@@ -0,0 +1,12 @@
+return {
+	[1] = {Tools = {"M4", "Glock 17", "Torch", "Mop"}, Morph = "Physics", Clothing = {5743682667, 5743682167}, HealthBonus = 25, TeamtagName = "Recruit"},
+	[2] = {Tools = {"M4", "Glock 17", "Torch", "Bandage", "Mop"}, Morph = "Physics", Clothing = {5743682667, 5743682167}, HealthBonus = 50, TeamtagName = "Operative"},
+	[3] = {Tools = {"M4", "Glock 17", "Torch", "Bandage", "Mop"}, Morph = "Physics", Clothing = {5743682667, 5743682167}, HealthBonus = 75, TeamtagName = "Corporal"},
+	[4] = {Tools = {"M4", "Glock 17", "Torch", "Bandage", "Mop"}, Morph = "Physics", Clothing = {5743682667, 5743682167}, HealthBonus = 100, TeamtagName = "Sergeant"},
+	[5] = {Tools = {"M4", "Glock 17", "Torch", "Bandage", "Mop"}, Morph = "Physics", Clothing = {5743682667, 5743682167}, HealthBonus = 125, TeamtagName = "Sergeant Master"},
+	[6] = {Tools = {"P90 SOW", "Glock 18", "Torch", "Bandage", "Mop"}, Morph = "EGOC", Clothing = {5743682667, 5743682167}, HealthBonus = 150, TeamtagName = "Senior Operative"},
+	[7] = {Tools = {"P90 SOW", "Glock 18", "Torch", "Bandage", "Mop"}, Morph = "EGOC", Clothing = {5743682667, 5743682167}, HealthBonus = 175, TeamtagName = "Officer"},
+	[8] = {Tools = {"P90 Elite", "Glock 18", "Torch", "Bandage", "Medkit", "Mop"}, Morph = "EGOC", Clothing = {5743682667, 5743682167}, HealthBonus = 200, TeamtagName = "Lieutenant Command"},
+	[9] = {Tools = {"P90 Elite", "Glock 18", "Torch", "Bandage", "Medkit", "Mop"}, Morph = "EGOC", Clothing = {5743682667, 5743682167}, HealthBonus = 225, TeamtagName = "Major Command"},
+	[10] = {Tools = {"SCAR-L", "KSG-11", "Glock 18", "Torch", "Bandage", "Medkit", "Mop"}, Morph = "HEAD108", Clothing = {5743682667, 5743682167}, HealthBonus = 250, TeamtagName = "Senior Leading Officer"},
+}
