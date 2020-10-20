68039965ed8c0b80a568583fa71beba9880e03a4
Fwogg3r <73174497+Fwogg3r@users.noreply.github.com>
2020-10-20 08:53:14 -0400

    Create Intelligence Agency.lua

---
+++ Intelligence Agency.lua
@@ -0,0 +1,12 @@
+return {
+	[1] = {Tools = {"Glock 17", "MP5", "Torch", "Cloak", "Mop"}, Morph = "IA", Clothing = {5744141181, 5744135515}, HealthBonus = 25, TeamtagName = "Enlist"},
+	[2] = {Tools = {"Glock 17", "MP5", "Torch", "Cloak", "Mop"}, Morph = "IA", Clothing = {5744141181, 5744135515}, HealthBonus = 50, TeamtagName = "Agent"},
+	[3] = {Tools = {"Glock 17", "MP5", "Torch", "Cloak", "Mop"}, Morph = "IA", Clothing = {5744141181, 5744135515}, HealthBonus = 75, TeamtagName = "Special Agent"},
+	[4] = {Tools = {"Glock 17", "Baton", "Honey Badger", "Torch", "Cloak", "Mop"}, Morph = "IA", Clothing = {5744141181, 5744135515}, HealthBonus = 100, TeamtagName = "Senior Agent"},
+	[5] = {Tools = {"Glock 17", "Baton", "Honey Badger", "Torch", "Cloak", "Mop"}, Morph = "IA", Clothing = {5744141181, 5744135515}, HealthBonus = 125, TeamtagName = "Management Agent"},
+	[6] = {Tools = {"Glock 17", "Baton", "Honey Badger", "Torch", "Cloak", "Mop"}, Morph = "IA", Clothing = {5744141181, 5744135515}, HealthBonus = 150, TeamtagName = "Intelligence Officer"},
+	[7] = {Tools = {"Desert Eagle", "Baton", "P90 Elite", "Torch", "Detain", "Cloak", "Mop"}, Morph = "IA", Clothing = {5744141181, 5744135515}, HealthBonus = 175, TeamtagName = "Intelligence Supervisor"},
+	[8] = {Tools = {"Desert Eagle", "Baton", "P90 Elite", "Torch", "Detain", "Cloak", "Mop"}, Morph = "IA", Clothing = {5744141181, 5744135515}, HealthBonus = 200, TeamtagName = "Overwatch"},
+	[9] = {Tools = {"Vanguard - Glock 17", "Baton", "G36 Elite", "Torch", "Detain", "Bandage", "Cloak", "Mop"}, Morph = "IA", Clothing = {5744141181, 5744135515}, HealthBonus = 225, TeamtagName = "Secretary"},
+	[10] = {Tools = {"Vanguard - Glock 17", "Baton", "G36 Elite", "Torch", "Detain", "Bandage", "Cloak", "Mop"}, Morph = "IA", Clothing = {5744141181, 5744135515}, HealthBonus = 250, TeamtagName = "Head of Intelligence"},
+}
