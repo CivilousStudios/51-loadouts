bae9921ca6fd35e6ad01f0b5be0c85c92e22532b
Fwogg3r <73174497+Fwogg3r@users.noreply.github.com>
2020-10-20 08:52:42 -0400

    Create Hostile Disposable Class.lua

---
+++ Hostile Disposable Class.lua
@@ -0,0 +1,12 @@
+return {
+	[1] = {Tools = {"MP5", "Glock 17", "Torch", "Bandage", "Mop"}, Morph = "", Clothing = {5741890128, 5741896619}, HealthBonus = 10, TeamtagName = "New Thug"},
+	[2] = {Tools = {"MP5", "Glock 17", "Torch", "Bandage", "Mop"}, Morph = "", Clothing = {5741890128, 5741896619}, HealthBonus = 15, TeamtagName = "Thug"},
+	[3] = {Tools = {"MP5", "Glock 17", "Torch", "Bandage", "Mop"}, Morph = "", Clothing = {5741890128, 5741896619}, HealthBonus = 20, TeamtagName = "Known Thug"},
+	[4] = {Tools = {"MP5", "Glock 17", "Torch", "Bandage", "Mop"}, Morph = "", Clothing = {5741890128, 5741896619}, HealthBonus = 25, TeamtagName = "Rioter Class D"},
+	[5] = {Tools = {"MP5", "Glock 18", "Torch", "Bandage", "Mop"}, Morph = "", Clothing = {5741890128, 5741896619}, HealthBonus = 30, TeamtagName = "Rioter Class E"},
+	[6] = {Tools = {"MP5", "Glock 18", "Torch", "Bandage", "Medkit", "Mop"}, Morph = "", Clothing = {5741890128, 5741896619}, HealthBonus = 35, TeamtagName = "Super Rioter"},
+	[7] = {Tools = {"MP5", "Glock 18", "Torch", "Bandage", "Medkit", "Mop"}, Morph = "", Clothing = {5741890128, 5741896619}, HealthBonus = 40, TeamtagName = "Bigdog"},
+	[8] = {Tools = {"MP5", "Glock 18", "Torch", "Bandage", "Medkit", "Mop"}, Morph = "", Clothing = {5741890128, 5741896619}, HealthBonus = 50, TeamtagName = "Bulldog"},
+	[9] = {Tools = {"MP5", "Glock 18", "Torch", "Bandage", "Medkit", "Mop"}, Morph = "", Clothing = {5741890128, 5741896619}, HealthBonus = 75, TeamtagName = "LightBulb"},
+	[10] = {Tools = {"MP5", "Glock 18", "Torch", "Bandage", "Medkit", "Mop"}, Morph = "", Clothing = {5741890128, 5741896619}, HealthBonus = 100, TeamtagName = "Kingpin"},
+}
