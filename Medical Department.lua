ecc2e31ee775e1cbf632a0384978a08fbd582a61
Fwogg3r <73174497+Fwogg3r@users.noreply.github.com>
2020-10-20 08:54:12 -0400

    Create Medical Department.lua

---
+++ Medical Department.lua
@@ -0,0 +1,12 @@
+return {
+	[1] = {Tools = {"Torch", "Medkit", "Mop"}, Morph = "", Clothing = {5742297766, 5743786618}, HealthBonus = 0, TeamtagName = "Intern"},
+	[2] = {Tools = {"Glock 17", "Torch", "Medkit", "Bandage", "Mop"}, Morph = "", Clothing = {5742297766, 5743786618}, HealthBonus = 5, TeamtagName = "Nurse"},
+	[3] = {Tools = {"Glock 17", "Torch", "Medkit", "Bandage", "Mop"}, Morph = "", Clothing = {5742299196, 5742300497}, HealthBonus = 10, TeamtagName = "Assistant Doctor"},
+	[4] = {Tools = {"Glock 17", "Defibrillator", "Torch", "Medkit", "Bandage", "Mop"}, Morph = "", Clothing = {5742299196, 5742300497}, HealthBonus = 15, TeamtagName = "Doctor"},
+	[5] = {Tools = {"Glock 17", "Defibrillator", "Torch", "Medkit", "Bandage", "Mop"}, Morph = "", Clothing = {5742299196, 5742300497}, HealthBonus = 20, TeamtagName = "Attending Physician"},
+	[6] = {Tools = {"Glock 17", "Defibrillator", "Torch", "Medkit", "Bandage", "Mop"}, Morph = "", Clothing = {5742299196, 5742300497}, HealthBonus = 25, TeamtagName = "Surgeon"},
+	[7] = {Tools = {"Glock 17", "Defibrillator", "Torch", "Medkit", "Bandage", "Mop"}, Morph = "", Clothing = {5742299196, 5742300497}, HealthBonus = 30, TeamtagName = "Chief Resident"},
+	[8] = {Tools = {"Glock 17", "Defibrillator", "Torch", "Medkit", "Bandage", "Mop"}, Morph = "", Clothing = {5742299196, 5742300497}, HealthBonus = 50, TeamtagName = "Board Advisor"},
+	[9] = {Tools = {"Desert Eagle", "Defibrillator", "Torch", "Medkit", "Bandage", "Mop"}, Morph = "", Clothing = {5742299196, 5742300497}, HealthBonus = 75, TeamtagName = "Board Member"},
+	[10] = {Tools = {"Desert Eagle", "Defibrillator", "Torch", "Medkit", "Bandage", "Mop"}, Morph = "", Clothing = {5742299196, 5742300497}, HealthBonus = 100, TeamtagName = "Board Executive"},
+}
