d53cb59dffcff6e10c39c334c1cb76b9e55effde
Fwogg3r <73174497+Fwogg3r@users.noreply.github.com>
2020-10-20 08:48:44 -0400

    Create Ethics Committee.lua

---
+++ Ethics Committee.lua
@@ -0,0 +1,12 @@
+return {
+	[1] = {Tools = {"Clipboard", "Mop"}, Morph = "", Clothing = {5743694141, 5743693611}, HealthBonus = 0, TeamtagName = "Intern"},
+	[2] = {Tools = {"Clipboard", "Mop"}, Morph = "", Clothing = {5743694141, 5743693611}, HealthBonus = 5, TeamtagName = "Employee"},
+	[3] = {Tools = {"Glock 17", "Clipboard", "Mop"}, Morph = "", Clothing = {5743694141, 5743693611}, HealthBonus = 10, TeamtagName = "Senior Employee"},
+	[4] = {Tools = {"Glock 17", "Clipboard", "Mop"}, Morph = "", Clothing = {5743684734, 5743686279}, HealthBonus = 15, TeamtagName = "Inspector"},
+	[5] = {Tools = {"Glock 17", "Clipboard", "Mop"}, Morph = "", Clothing = {5743684734, 5743686279}, HealthBonus = 20, TeamtagName = "Inspector Supervisor"},
+	[6] = {Tools = {"Glock 18", "Clipboard", "Mop", "Detain"}, Morph = "", Clothing = {5743684734, 5743686279}, HealthBonus = 25, TeamtagName = "Senior Inspector"},
+	[7] = {Tools = {"Glock 18", "Clipboard", "Mop", "Detain"}, Morph = "", Clothing = {5743684734, 5743686279}, HealthBonus = 30, TeamtagName = "Inspector Board"},
+	[8] = {Tools = {"Glock 18", "Clipboard", "Mop", "Detain"}, Morph = "", Clothing = {5743684734, 5743686279}, HealthBonus = 50, TeamtagName = "Management Board"},
+	[9] = {Tools = {"Vanguard - Glock 17", "Clipboard", "Mop", "Detain"}, Morph = "", Clothing = {5743684734, 5743686279}, HealthBonus = 75, TeamtagName = "Secretary of Human Resources"},
+	[10] = {Tools = {"MP5", "Vanguard - Glock 17", "Clipboard", "Mop", "Detain"}, Morph = "", Clothing = {5743684734, 5743686279}, HealthBonus = 100, TeamtagName = "Inspector General"},
+}
