dcd65dfb54dfefd62629501939bfe62a3fb6a21d
Fwogg3r <73174497+Fwogg3r@users.noreply.github.com>
2020-10-20 08:56:18 -0400

    Create Phantom Task Force.lua

---
+++ Phantom Task Force.lua
@@ -0,0 +1,12 @@
+return {
+	[1] = {Tools = {"Level 4 Keycard", "SCAR-L", "Glock 18", "Baton", "Bandage", "Medkit", "Detain", "Mop"}, Morph = "Aux", Clothing = {5743678986, 5743679600}, HealthBonus = 150, TeamtagName = "Phantom Auxillary"},
+	[2] = {Tools = {"Level 4 Keycard", "SCAR-L", "Remington 700", "Glock 18", "Baton", "Bandage", "Medkit", "Detain", "Mop"}, Morph = "Aux", Clothing = {5743678986, 5743679600}, HealthBonus = 200, TeamtagName = "Phantom Operative"},
+	[3] = {Tools = {"Level 4 Keycard", "SCAR-H", "Remington 700", "Glock 18", "Baton", "Bandage", "Medkit", "Detain", "Mop"}, Morph = "Aux", Clothing = {5743678986, 5743679600}, HealthBonus = 250, TeamtagName = "Phantom Corporal"},
+	[4] = {Tools = {"Level 4 Keycard", "SCAR-H", "Remington 700", "Glock 18", "Baton", "Bandage", "Medkit", "Detain", "Mop"}, Morph = "Aux", Clothing = {5743678986, 5743679600}, HealthBonus = 300, TeamtagName = "Phantom Sergeant"},
+	[5] = {Tools = {"Level 4 Keycard", "SCAR-H", "M249", "Remington 700", "Glock 18", "Baton", "Bandage", "Detain", "Medkit", "Mop"}, Morph = "Aux", Clothing = {5743678986, 5743679600}, HealthBonus = 350, TeamtagName = "Phantom Specalist"},
+	[6] = {Tools = {"Level 4 Keycard", "SCAR-H", "M249", "Remington 700", "Glock 18", "Baton", "Bandage", "Detain", "Medkit", "Mop"}, Morph = "Aux", Clothing = {5743678986, 5743679600}, HealthBonus = 400, TeamtagName = "Phantom Officer"},
+	[7] = {Tools = {"Level 5 Keycard", "SCAR-H", "M249", "Remington 700", "Glock 18", "Baton", "Bandage", "Detain", "Medkit", "Mop"}, Morph = "Aux", Clothing = {5743678986, 5743679600}, HealthBonus = 450, TeamtagName = "Phantom Lieutenant"},
+	[8] = {Tools = {"Level 5 Keycard", "SCAR-H", "M249", "Remington 700", "Glock 18", "Baton", "Bandage", "Detain", "Medkit", "Mop"}, Morph = "Aux", Clothing = {5743678986, 5743679600}, HealthBonus = 500, TeamtagName = "Phantom Captain"},
+	[9] = {Tools = {"Level 5 Keycard", "G36 Elite", "M60", "Remington 700", "Glock 18", "Baton", "Bandage", "Detain", "Medkit", "Mop"}, Morph = "COM+", Clothing = {5743678986, 5743679600}, HealthBonus = 550, TeamtagName = "Phantom Major"},
+	[10] = {Tools = {"Level 5 Keycard", "G36 Elite", "M60", "Remington 700", "Glock 18", "Baton", "Bandage", "Detain", "Medkit", "Mop"}, Morph = "COM+", Clothing = {5743678986, 5743679600}, HealthBonus = 600, TeamtagName = "Phantom Colonel"},
+}
