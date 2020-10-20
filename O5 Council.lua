2b439de13baf79c9780ab5bef5a6488938f3816f
Fwogg3r <73174497+Fwogg3r@users.noreply.github.com>
2020-10-20 08:55:49 -0400

    Create O5 Council.lua

---
+++ O5 Council.lua
@@ -0,0 +1,12 @@
+return {
+	[1] = {Tools = {"Level 5 Keycard", "Desert Eagle", "MP5", "Bandage", "Torch", "Detain", "Mop"}, Morph = "", Clothing = {5743677092, 5743678048}, HealthBonus = 150, TeamtagName = "O5 - 10"},
+	[2] = {Tools = {"Level 5 Keycard", "Desert Eagle", "MP5", "Bandage", "Torch", "Detain", "Mop"}, Morph = "", Clothing = {5743677092, 5743678048}, HealthBonus = 200, TeamtagName = "O5 - 9"},
+	[3] = {Tools = {"Level 5 Keycard", "Desert Eagle", "MP5", "Bandage", "Torch", "Detain", "Mop"}, Morph = "", Clothing = {5743677092, 5743678048}, HealthBonus = 250, TeamtagName = "O5 - 8"},
+	[4] = {Tools = {"Level 5 Keycard", "Desert Eagle", "MP5", "Bandage", "Torch", "Detain", "Mop"}, Morph = "", Clothing = {5743677092, 5743678048}, HealthBonus = 300, TeamtagName = "O5 - 7"},
+	[5] = {Tools = {"Level 5 Keycard", "Desert Eagle", "MP5", "Bandage", "Torch", "Detain", "Mop"}, Morph = "", Clothing = {5743677092, 5743678048}, HealthBonus = 350, TeamtagName = "O5 - 6"},
+	[6] = {Tools = {"Level 5 Keycard", "Desert Eagle", "MP5", "Bandage", "Torch", "Detain", "Mop"}, Morph = "", Clothing = {5743677092, 5743678048}, HealthBonus = 400, TeamtagName = "O5 - 5"},
+	[7] = {Tools = {"Level 5 Keycard", "Desert Eagle", "MP5", "Bandage", "Torch", "Detain", "Mop"}, Morph = "", Clothing = {5743677092, 5743678048}, HealthBonus = 450, TeamtagName = "O5 - 4"},
+	[8] = {Tools = {"Level 5 Keycard", "Desert Eagle", "RPK", "Bandage", "Torch", "Detain", "Mop"}, Morph = "", Clothing = {5743677092, 5743678048}, HealthBonus = 500, TeamtagName = "O5 - 3"},
+	[9] = {Tools = {"Level 5 Keycard", "Desert Eagle", "RPK", "Bandage", "Torch", "Detain", "Mop"}, Morph = "", Clothing = {5743677092, 5743678048}, HealthBonus = 550, TeamtagName = "O5 - 2"},
+	[10] = {Tools = {"Level 5 Keycard", "Desert Eagle", "RPK", "Bandage", "Torch", "Detain", "Mop"}, Morph = "", Clothing = {5743677092, 5743678048}, HealthBonus = 600, TeamtagName = "O5 - X"},
+}
