08590af4771d21e9188ce1af6c0af484b8a4b9f8
Fwogg3r <73174497+Fwogg3r@users.noreply.github.com>
2020-10-20 08:53:49 -0400

    Create Internal Security.lua

---
+++ Internal Security.lua
@@ -0,0 +1,12 @@
+return {
+	[1] = {Tools = {"Glock 18", "Honey Badger", "Torch", "Cloak", "Mop"}, Morph = "ISD", Clothing = {5748228861, 5743756522}, HealthBonus = 25, TeamtagName = "Agent"},
+	[2] = {Tools = {"Glock 18", "Honey Badger", "Torch", "Cloak", "Mop"}, Morph = "ISD", Clothing = {5748228861, 5743756522}, HealthBonus = 50, TeamtagName = "Special Agent"},
+	[3] = {Tools = {"Glock 18", "Honey Badger", "Torch", "Cloak", "Mop"}, Morph = "ISD", Clothing = {5748228861, 5743756522}, HealthBonus = 75, TeamtagName = "Special Agent First Class"},
+	[4] = {Tools = {"Glock 18", "G36C", "Baton", "Torch", "Cloak", "Mop"}, Morph = "ISD", Clothing = {5748228861, 5743756522}, HealthBonus = 100, TeamtagName = "Senior Agent"},
+	[5] = {Tools = {"Glock 18", "G36C", "Baton", "Torch", "Cloak", "Mop"}, Morph = "ISD", Clothing = {5748228861, 5743756522}, HealthBonus = 125, TeamtagName = "Supervisor Agent"},
+	[6] = {Tools = {"Glock 18", "G36C", "Baton", "Torch", "Cloak", "Mop"}, Morph = "ISD", Clothing = {5748228861, 5743756522}, HealthBonus = 150, TeamtagName = "Board of Security"},
+	[7] = {Tools = {"Desert Eagle", "P90 SOW", "Baton", "Torch", "Detain", "Cloak", "Mop"}, Morph = "ISD", Clothing = {5748228861, 5743756522}, HealthBonus = 175, TeamtagName = "Internal Management"},
+	[8] = {Tools = {"Desert Eagle", "P90 SOW", "Baton", "Torch", "Detain", "Cloak", "Mop"}, Morph = "ISD", Clothing = {5748228861, 5743756522}, HealthBonus = 200, TeamtagName = "Board of Internal Security"},
+	[9] = {Tools = {"Vanguard - Glock 17", "Baton", "G36 Elite", "Torch", "Detain", "Bandage", "Cloak", "Mop"}, Morph = "ISD", Clothing = {5748228861, 5743756522}, HealthBonus = 225, TeamtagName = "Internal Command"},
+	[10] = {Tools = {"Vanguard - Glock 17", "Baton", "G36 Elite", "Torch", "Detain", "Bandage", "Cloak", "Mop"}, Morph = "ISD", Clothing = {5748228861, 5743756522}, HealthBonus = 250, TeamtagName = "Head Senior Agent of 51"},
+}
