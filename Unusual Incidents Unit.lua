d2faaeb122cb184e6e50e04cc01f8c0de691847b
Fwogg3r <73174497+Fwogg3r@users.noreply.github.com>
2020-10-20 08:58:09 -0400

    Create Unusual Incidents Unit.lua

---
+++ Unusual Incidents Unit.lua
@@ -0,0 +1,12 @@
+return {
+	[1] = {Tools = {"Level 4 Keycard", "MP5", "Glock 18", "Baton", "Bandage", "Medkit", "Detain", "Mop"}, Morph = "UIU+", Clothing = {5743699324, 5743754036}, HealthBonus = 150, TeamtagName = "Probationary Field Agent"},
+	[2] = {Tools = {"Level 4 Keycard", "MP5", "Remington 700", "Glock 18", "Baton", "Bandage", "Medkit", "Detain", "Mop"}, Morph = "UIU+", Clothing = {5743699324, 5743754036}, HealthBonus = 200, TeamtagName = "Field Agent"},
+	[3] = {Tools = {"Level 4 Keycard", "M4", "Remington 700", "Glock 18", "Baton", "Bandage", "Medkit", "Detain", "Mop"}, Morph = "UIU+", Clothing = {5743699324, 5743754036}, HealthBonus = 250, TeamtagName = "Senior Field Agent"},
+	[4] = {Tools = {"Level 4 Keycard", "M4", "Remington 700", "Glock 18", "Baton", "Bandage", "Medkit", "Detain", "Mop"}, Morph = "UIU+", Clothing = {5743699324, 5743754036}, HealthBonus = 300, TeamtagName = "Specialist"},
+	[5] = {Tools = {"Level 4 Keycard", "G36", "M249", "Remington 700", "Glock 18", "Baton", "Bandage", "Detain", "Medkit", "Mop"}, Morph = "UIUDMT+", Clothing = {5743699324, 5743754036}, HealthBonus = 350, TeamtagName = "Special Agent"},
+	[6] = {Tools = {"Level 4 Keycard", "G36", "M249", "Remington 700", "Glock 18", "Baton", "Bandage", "Detain", "Medkit", "Mop"}, Morph = "UIUDMT+", Clothing = {5743699324, 5743754036}, HealthBonus = 400, TeamtagName = "Senior Special Agent"},
+	[7] = {Tools = {"Level 5 Keycard", "G36C", "M249", "Remington 700", "Glock 18", "Baton", "Bandage", "Detain", "Medkit", "Mop"}, Morph = "UIUDMT+", Clothing = {5743699324, 5743754036}, HealthBonus = 450, TeamtagName = "Supervisory Special Agent"},
+	[8] = {Tools = {"Level 5 Keycard", "G36C", "M249", "Remington 700", "Glock 18", "Baton", "Bandage", "Detain", "Medkit", "Mop"}, Morph = "UIUDD+", Clothing = {5743699324, 5743754036}, HealthBonus = 500, TeamtagName = "Board of Directors"},
+	[9] = {Tools = {"Level 5 Keycard", "G36 Elite", "M249", "Remington 700", "Glock 18", "Baton", "Bandage", "Detain", "Medkit", "Mop"}, Morph = "UIUDD+", Clothing = {5743699324, 5743754036}, HealthBonus = 550, TeamtagName = "Deputy Director"},
+	[10] = {Tools = {"Level 5 Keycard", "G36 Elite", "M249", "Remington 700", "Glock 18", "Baton", "Bandage", "Detain", "Medkit", "Mop"}, Morph = "UIUDD+", Clothing = {5743699324, 5743754036}, HealthBonus = 600, TeamtagName = "Director"},
+}
