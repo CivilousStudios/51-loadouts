85823b4f5d5eaff2e9486a3a0c3ab52ca352cb71
Fwogg3r <73174497+Fwogg3r@users.noreply.github.com>
2020-10-20 08:57:09 -0400

    Create Scientific Department.lua

---
+++ Scientific Department.lua
@@ -0,0 +1,12 @@
+return {
+	[1] = {Tools = {"Mop", "Clipboard"}, Morph = "", Clothing = {5743697603, 5743751964}, HealthBonus = 0, TeamtagName = "Intern"},
+	[2] = {Tools = {"Mop", "Clipboard"}, Morph = "", Clothing = {5743697603, 5743751964}, HealthBonus = 5, TeamtagName = "Assistant Researcher"},
+	[3] = {Tools = {"Glock 17", "Mop", "Clipboard", "Mop"}, Morph = "", Clothing = {5743697603, 5743751964}, HealthBonus = 10, TeamtagName = "Researcher"},
+	[4] = {Tools = {"Glock 17", "Mop", "Clipboard", "Mop"}, Morph = "", Clothing = {5743697603, 5743751964}, HealthBonus = 15, TeamtagName = "Senior Researcher"},
+	[5] = {Tools = {"Glock 17", "Mop", "Clipboard", "Torch", "Mop"}, Morph = "", Clothing = {5743697603, 5743751964}, HealthBonus = 20, TeamtagName = "Analyst"},
+	[6] = {Tools = {"Glock 18", "Mop", "Clipboard", "Torch", "Mop"}, Morph = "", Clothing = {5743697603, 5743751964}, HealthBonus = 25, TeamtagName = "Specalized Researcher"},
+	[7] = {Tools = {"Glock 18", "Mop", "Clipboard", "Torch", "Mop"}, Morph = "", Clothing = {5743697603, 5743751964}, HealthBonus = 30, TeamtagName = "Research Board Member"},
+	[8] = {Tools = {"Glock 18", "Mop", "Clipboard", "Torch", "Mop"}, Morph = "", Clothing = {5743697603, 5743751964}, HealthBonus = 50, TeamtagName = "Research Board Advisor"},
+	[9] = {Tools = {"Glock 18", "Mop", "Clipboard", "Torch", "Mop"}, Morph = "", Clothing = {5743697603, 5743751964}, HealthBonus = 75, TeamtagName = "Research Board Council Advisor"},
+	[10] = {Tools = {"Glock 18", "Mop", "Clipboard", "Torch", "Mop"}, Morph = "", Clothing = {5743697603, 5743751964}, HealthBonus = 100, TeamtagName = "Board Councilor"},
+}
